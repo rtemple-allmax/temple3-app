f1c3fa9858585da20c77c75d611e6effb36e14b1
developer-temple <52588885+developer-temple@users.noreply.github.com>
2023-07-20 11:33:36 -0400

    Updated during testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,45 +14,9 @@ class AppComponent extends Component<Props, State> {
   
   protected afterRender(): void {
     const switcher = this.root?.getElementById('switch-btn');
-    // this.showGrid(switcher);
     (this.root?.getElementById('ribbon') as RibbonComponent)?.configure(ribbonDef);
     (this.root?.getElementById('data-view') as DataViewComponent)?.configure(config);
-    // (this.root?.getElementById('chart') as DataChartComponent)?.hydrate(config.data);
-    // (this.root?.getElementById('test-text-box') as TextBoxComponent)?.addEventListener('valueChanged', (e: CustomEvent) => {
-    //   const testTarget = this.root.getElementById('test-target');
-    //   if (testTarget) {
-    //     testTarget.innerHTML = `This value is set during custom event listener in App Component - ${ e.detail }`;
-    //   }
-    // })
-    
-    // if (switcher) {
-    //   switcher.addEventListener('click', () => {
-    //     if (this.gridVisible) {
-    //       this.showChart(switcher);
-    //     } else {
-    //       this.showGrid(switcher);
-    //     }
-    //   });
-    // }
-  }
-
-  // private showGrid(btn?: HTMLElement): void {
-  //   (this.root?.getElementById('grid-if') as IfComponent)?.visible(true);
-  //   (this.root?.getElementById('chart-if') as IfComponent)?.visible(false);
-  //   this.gridVisible = true;
-  //   if (btn) {
-  //     btn.textContent = 'Show Chart';
-  //   }
-  // }
-
-  // private showChart(btn?: HTMLElement): void {
-  //   (this.root?.getElementById('grid-if') as IfComponent)?.visible(false);
-  //   (this.root?.getElementById('chart-if') as IfComponent)?.visible(true);
-  //   this.gridVisible = false;
-  //   if (btn) {
-  //     btn.textContent = 'Show Table';
-  //   }
-  // }
+  }  
   
   private manageTheme(): void {
     new ThemeService().customize({
